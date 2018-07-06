321bba4e3ae31f6407f3c818e81c0340b700660a
josualicea <40583891+josualicea@users.noreply.github.com>
2018-07-06 16:50:54 -0700

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World");		
 		System.out.println("Adding code to this program");
+		System.out.println("Adding code from GitHub");
 	}
 }
